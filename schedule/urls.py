d1218d26c1a94981fbf4be9560985a4bf6c6c4d2
zlyu88 <zlyu88@gmail.com>
2016-12-22 14:25:24 +0200

    add base structure

--- schedule/urls.py
+++ schedule/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
-#
+    url(r'^$', views.log_in, name='log_in'),
+
+    url(r'^base/(?P<uid>\d+)/$', views.base, name='base'),
+    url(r'^room_page/(?P<rid>\d+)/$', views.room_page, name='room_page'),
 
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -8,4 +8,41 @@ from .models import User, Room
 from .forms import LogIn
 
 
-#
+def log_in(request):
+    if request.method == 'POST':
+        form = LogIn(request.POST)
+        if form.is_valid():
+            try:
+                user = User.objects.get(name=form.cleaned_data['name'],
+                                        password=form.cleaned_data['password'])
+                user.is_logged = True
+                user.save()
+                url = reverse('base', kwargs={'uid': user.id})
+                return HttpResponseRedirect(url)
+            except User.DoesNotExist:
+                return HttpResponseRedirect('/schedule/log_in/')
+    else:
+        form = LogIn()
+    return render(request, 'login.html', {'form': form})
+
+
+def base(request, uid=None):
+    users = User.objects.all()
+    rooms = Room.objects.all()
+    try:
+        user = User.objects.get(id=uid)
+    except User.DoesNotExist:
+        user = None
+    context = {'user': user, 'users': users, 'rooms': rooms}
+    return render(request, 'index.html', context)
+
+
+def room_page(request, rid):
+    room = Room.objects.get(id=rid)
+    users = User.objects.all()
+    context = {'users': users, 'room': room}
+    return render(request, 'room_page.html', context)
+
+
+def test(request, uid):
+    return HttpResponse('link works with id: %s' % uid)
